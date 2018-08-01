934c5f91a38f58a6b1819d63870091496bcb468d
Mpho Mphego <mmphego@ska.ac.za>
2018-08-01 10:42:23 +0200

    Updated README file and added various dependency files

--- README.md
+++ README.md
@@ -4,41 +4,30 @@ AVN Correlator Unit and Acceptance Testing based Framework
 
 ## Installation
 
-### Debian packages
-
-Install dependencies:
-
-    $ # sudo apt-get update && sudo apt-get install -yfm $(cat apt-build-requirements.txt)
-
 ### Python Core packages
 
 Install dependencies to the system, by following their installation instructions:
 
-* [_katcp-python_](https://github.com/ska-sa/katcp-python)
+* [_katcp-python_](https://github.com/ska-sa/katcp-python) [optional]
 * [_nosekatreport_](https://github.com/ska-sa/nosekatreport/tree/karoocbf)
 
 ### Python testing dependencies packages
 
-It is highly advisable to install these dependencies on a [_Python virtual environment_](https://virtualenv.pypa.io/), below is step-by-step instructions.
-#### Setup and Use Virtualenv
+It is highly recommended to install dependencies on a [_Python virtual environment_](https://virtualenv.pypa.io/).
+
+#### Setup Virtualenv
+Step-by-step instructions.
 ```
 # Install Python essentials and pip
 $ curl -s https://bootstrap.pypa.io/get-pip.py | python
-$ sudo pip install virtualenv virtualenvwrapper
+$ pip install --user virtualenv
 
 # Install testing dependencies in the virtualenv
-$ virtualenv venv
-$ source ~/venv/bin/activate
+$ git clone https://github.com/ska-sa/AVNTests
 $ cd AVNTests
-$ pip install -r pip-requirements
-
-```
-
-### AVNTests installation (Optional)
-Install AVN Tests 
-```
-$ . venv/bin/activate
-$ python setup.py install
+$ virtualenv .venv
+$ . .venv/bin/activate
+$ $(which pip) install -e .
 ```
 
 ## Unit Testing
---
+++ setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# https://stackoverflow.com/a/44077346
+import atexit
+import os
+
+from contextlib import contextmanager
+from distutils.command.install import install
+from glob import glob
+
+# https://stackoverflow.com/a/49867265
+try:
+    from pip.download import PipSession
+except ImportError:
+    from pip._internal.download import PipSession
+
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
+
+from setuptools import setup
+from setuptools import find_packages
+from subprocess import check_output
+from subprocess import CalledProcessError
+from warnings import filterwarnings
+
+
+# I do not condone it, but we surpressing all warnings
+filterwarnings('ignore')
+
+# Latest tagged stable version
+try:
+  __version__ = check_output(["git", "describe", "--tags"]).rstrip().split('-')[-1]
+except CalledProcessError:
+  __version__ = '0.1'
+
+# Install dependencies with failover/failsafe
+try:
+    install_reqs = parse_requirements("pip-requirements.txt",  session=PipSession())
+    __install_requires__ = [str(ir.req) for ir in install_reqs]
+    try:
+        __install_requires__.remove('None')
+    except ValueError:
+        pass
+except Exception:
+    __install_requires__ = [
+        'matplotlib', 'numpy', 'h5py', 'Nose', 'ansicolor', 'traceback2']
+
+@contextmanager
+def cwd(path):
+    oldpwd=os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+class cmdatexit_install(install):
+    """"""
+    def run(self):
+        path = ''
+        install.run(self)
+        if os.path.exists(path):
+            with cwd(path):
+                print 'Installing %s from %s' % (path, os.getcwd())
+
+
+setup(name='AVNTests',
+      version=__version__,
+      description='AVN Correlator Unit and Acceptance Testing based Framework',
+      long_description=open('README.md').read(),
+      license='GPL',
+      author='SKA SA DBE Team',
+      author_email='mmphego@ska.ac.za',
+      url='https://github.com/ska-sa/AVNTests',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Radio Telescope correlator builders',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        ],
+      install_requires=__install_requires__,
+      dependency_links=['https://github.com/ska-sa/nosekatreport/tree/karoocbf'],
+      provides=['AVNTests'],
+      packages=find_packages(),
+      scripts=glob('scripts/*'),
+      # cmdclass={"install": cmdatexit_install, },
+      )
+
+# end
